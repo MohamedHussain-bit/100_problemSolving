3c0e2da3a6f0436859c4265bc9d36c919348fb0e
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-21 19:37:51 +0300

    add Anather solution

--- repete_str.js
+++ repete_str.js
@@ -12,3 +12,14 @@ function repetString(num , str){
 }
 
 console.log(repetString(5 , " M "))
+
+//todo :=> Anather solution
+
+function repetString2(num2 , str2){
+    return str2.repeat(num2)
+
+}
+
+console.log(repetString2(5 , " M ")) 
+
+
