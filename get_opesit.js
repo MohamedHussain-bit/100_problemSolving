0cf3fe58198d1d121adf01fff9fc333cfac39458
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-22 17:42:49 +0300

    add anather solution

--- get_opesit.js
+++ get_opesit.js
@@ -7,3 +7,12 @@ function gitOpesite(number){
 
 console.log(gitOpesite(10))
 
+// todo : => Anather solution
+
+function gitOpesite2(number2){
+    return  -number2  
+
+}
+
+console.log(gitOpesite2(10))
+
