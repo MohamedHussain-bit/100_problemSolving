b61d236b7db3f126b861351728f5ea4f80b68a10
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-22 16:26:06 +0300

    add the file convert_number_to_reversd_array.js

---
+++ convert_number_to_reversd_array.js
@@ -0,0 +1,12 @@
+// change the number to string
+// split the string to array
+// change the string in array in number
+// reverse to array
+ function digitin(n){
+    return n.toString()
+    .splite("")
+    .map((m) => Number(m))
+    .reverse()
+ }
+
+ console.log(digitin(12345))
