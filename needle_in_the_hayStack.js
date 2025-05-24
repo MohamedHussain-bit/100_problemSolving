25578c2f199c2083c420bca7a2d39742da145b2d
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-24 15:03:43 +0300

    add afile whith solve problem  foundIndex_needle

---
+++ needle_in_the_hayStack.js
@@ -0,0 +1,7 @@
+//! founde the index of the elemnt needle in array
+
+function foundIndex_needle(array){
+    return array.indexOf("needle")
+}
+
+console.log(foundIndex_needle(["medo","body","hamode","koko","spider","needle","nono"]))
