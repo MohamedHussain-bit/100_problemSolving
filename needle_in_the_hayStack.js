5ca9cd2f487e8d1bcad28d89ff35ba1e54566aac
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-24 15:18:47 +0300

    add anather solution

--- needle_in_the_hayStack.js
+++ needle_in_the_hayStack.js
@@ -1,7 +1,19 @@
 //! founde the index of the elemnt needle in array
 
 function foundIndex_needle(array){
-    return array.indexOf("needle")
+    return "the index of needle = " + array.indexOf("needle")
 }
 
 console.log(foundIndex_needle(["medo","body","hamode","koko","spider","needle","nono"]))
+
+// todo : => Anather solution
+
+function foundIndex_needle2(array){
+    for (let i = 0 ; i < array.length ; i++){
+        if (array[i] === "needle"){
+            return "the index of needle = " + i
+        }
+    }
+}
+
+console.log(foundIndex_needle2(["medo","body","hamode","koko","spider","needle","nono"]))
