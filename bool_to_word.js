2ecb443915df0ccf236c3f2f83742840ea09e9d7
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-25 15:55:48 +0300

    add a file the solve problem to bool to worde

---
+++ bool_to_word.js
@@ -0,0 +1,11 @@
+// cheack if word trou return yes else return no
+
+function boolToWord(bool){
+    if(bool == true){
+        return "Yes"
+    } else {
+        return "No"
+    }
+}
+
+console.log(boolToWord(true))
