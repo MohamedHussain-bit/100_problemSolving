2ba50205b3949b9228b74479cefe6793ff2acf03
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-26 23:24:35 +0300

    add a file solve the problem to switch it up

---
+++ switch_it_up.js
@@ -0,0 +1,39 @@
+//switch it up 
+
+function switchItUp(number){
+    switch(number){
+        case 0 :
+            return "Zero"
+            break;
+         case 2 :
+            return "Two"
+            break;
+         case 3 :
+            return "Three"
+            break;
+         case 4 :
+            return "Four"
+            break;
+         case 5 :
+            return "Five"
+            break;
+         case 6 :
+            return "Six"
+            break;
+         case 7 :
+            return "Seven"
+            break;
+         case 8 :
+            return "Aight"
+            break;
+         case 9 :
+            return "Nine"
+            break;
+        default :
+            return "Wrong number"
+         
+    }
+
+}
+
+console.log(switchItUp(6))
