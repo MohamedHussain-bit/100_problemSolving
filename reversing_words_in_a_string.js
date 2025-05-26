9c4c41465e62d4e873a0477804ec068351acb408
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-26 17:03:12 +0300

    add a file the solve problem to reversing words in a string

---
+++ reversing_words_in_a_string.js
@@ -0,0 +1,9 @@
+// turn string to array
+// reverse array
+// return array to string
+
+function reversWorde(string){
+    return string.split(" ").reverse().join(" ")
+}
+
+console.log(reversWorde("hello world"))
