a030394192bbe903adbedb717d0af44f75d861a9
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-26 17:29:59 +0300

    add anather solution

--- convert_a_number_to_a_string.js
+++ convert_a_number_to_a_string.js
@@ -6,3 +6,11 @@ function toString(number){
 
 console.log(typeof(toString(100)))
 
+// todo : => Anather solution
+
+function toString2(number){
+    return `${number}`
+}
+
+console.log(typeof(toString2(100)))
+
