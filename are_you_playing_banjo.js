23e019c1d53db9020a8f087d3c5d75306b6815f4
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-27 16:15:49 +0300

    add a file the solve problem to plaing banjo

---
+++ are_you_playing_banjo.js
@@ -0,0 +1,12 @@
+// are you plaing banjo
+
+function blaingBanjo(name){
+    if(name.charAt(0) === "R" || "r"){
+        return `${name} plaing banjo`
+    } else {
+        return `${name} no plaing banjo`
+    }
+
+}
+
+console.log("Ream")
