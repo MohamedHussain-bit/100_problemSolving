7bb5d7eb81d366371ed51942a006c9349c0db462
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-28 14:10:05 +0300

    add a file the solve peoblem to volume of a cuboid

---
+++ volume_of_a_cuboid.js
@@ -0,0 +1,10 @@
+// get volume of cuboide
+
+class kata{
+    static git_volume_of_cuboid(length , width , height){
+        return length * width * height
+    }
+
+}
+
+console.log(kata.git_volume_of_cuboid(10 , 10 , 10))
