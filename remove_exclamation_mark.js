f89806675742c74e84e64cbbe83fb50dac2a36c9
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-31 10:17:23 +0300

    add anather solution

--- remove_exclamation_mark.js
+++ remove_exclamation_mark.js
@@ -5,3 +5,13 @@ function remove(string){
 }
 
 console.log(remove("Hell!o Wo!rled !!!"))
+
+
+// todo : => Anather solution
+
+function remove2(string){
+    return string.split("").filter((n) => n !== "!").join("")
+
+}
+
+console.log(remove2("Hell!o Wo!rled !!!"))
