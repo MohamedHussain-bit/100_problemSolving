585fbd48c7d1e4a4a7da019047e47140a4ec4a3e
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-05-31 16:00:16 +0300

    add a file to solve the problem is in dividsble

---
+++ is_in_devesable.js
@@ -0,0 +1,13 @@
+// is in devesable
+
+function isDivisble(n , y , x){
+    if(n % y == 0 && n % x == 0){
+        return true
+    } else{
+        return false
+    }
+
+}
+
+console.log(isDivisble(12 , 6 , 3))
+
