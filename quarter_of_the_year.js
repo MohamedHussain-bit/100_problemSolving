2deaca8c30ef3dec7d93ca1350c5ed65afc9f48a
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-01 13:46:57 +0300

    add edet

--- quarter_of_the_year.js
+++ quarter_of_the_year.js
@@ -1,7 +1,7 @@
 // quarter of the year
 
 function ofQuaeter(month){
-    return month <= 4 ? 1 : month <= 7 ? 2 : month <= 10 ? 3 : 4
+    return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4
 
 }
 
