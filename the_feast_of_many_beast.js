cbe92bf0e6fb86d96962b5393b477f0287148de0
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-01 12:25:40 +0300

    add a file solve the problem to the feast of many beast

---
+++ the_feast_of_many_beast.js
@@ -0,0 +1,9 @@
+// the feast of many beast
+
+function feasts(beast , dish){
+    return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+
+}
+
+console.log(feasts("brawn bear" , "bear claw"))
+
